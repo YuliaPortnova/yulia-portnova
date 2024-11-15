ebc4be4f9a87ae4bc3ae324b0ca9b467c09c9dbf
Yulia Portnova <yuyul@yandex.ru>
2024-11-15 12:03:33 +0300

    feat: возвращает публикацию через папку docs, настраивает линтеры

---
+++ docs/scripts/main-menu.js
@@ -0,0 +1,1 @@
+var e=document.querySelector(".main-header"),o=e.querySelector(".main-header__toggle"),t=()=>{let n=()=>{e.classList.toggle("is-open")};o.addEventListener("click",n)};export{t as initMainMenu};
