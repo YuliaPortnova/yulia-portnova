111867957e3261c92f4e6324fae30ab9e6641764
Yulia Portnova <yuyul@yandex.ru>
2024-11-22 11:57:00 +0300

    feat: добавляет анимации про скролле WIP

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,5 +1,9 @@
 import { initMainMenu } from './main-menu.js';
 import { initAnimation } from './animation.js';
+import { initStickyHeader } from './sticky-header.js';
+import { initProgressBar } from './progress-bar.js';
 
-initMainMenu();
 initAnimation();
+initStickyHeader();
+initProgressBar();
+initMainMenu();
---
+++ source/scripts/progress-bar.js
@@ -0,0 +1,18 @@
+const progressBar = document.querySelector('.animation__progress-bar');
+
+const initProgressBar = () => {
+  if (!progressBar) {
+    return null;
+  }
+
+  const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
+
+  window.addEventListener('scroll', () => {
+    const windowScroll = window.scrollY;
+    const progressBarWidth = (windowScroll / windowHeight).toFixed(2);
+
+    progressBar.style.transform = `scaleX(${progressBarWidth})`;
+  });
+};
+
+export { initProgressBar };
