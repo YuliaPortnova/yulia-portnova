df70775a2260b49525f7f391000b5c02e8b5266b
Yulia Portnova <yuyul@yandex.ru>
2024-11-25 10:57:43 +0300

    feat: добавляет анимацию появления карточек при скролле

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,9 +1,9 @@
 import { initMainMenu } from './main-menu.js';
 import { initAnimation } from './animation.js';
-import { initStickyHeader } from './sticky-header.js';
+import { initScrollAnimation } from './scroll-animation.js';
 import { initProgressBar } from './progress-bar.js';
 
 initAnimation();
-initStickyHeader();
+initScrollAnimation();
 initProgressBar();
 initMainMenu();
