491a7675966979ff7c97c68e3d9c9538f67eda96
Yulia Portnova <yuyul@yandex.ru>
2024-11-25 12:26:07 +0300

    refactor: упрощает и сокращает js для анимации скролла

--- source/scripts/scroll-animation.js
+++ source/scripts/scroll-animation.js
@@ -1,50 +1,44 @@
 const headerElement = document.querySelector('.main-header:not(.main-header--not-sticky)');
-const projectElements = document.querySelectorAll('.projects__project');
+const animateAppearanceElements = document.querySelectorAll('.projects__project');
 const SCROLLED_HEADER_START = 500;
 const Y_TRANSLATION = 120;
 const screenHeight = document.documentElement.clientHeight;
 
 let scrollStarted = 0;
 
-const initScrollAnimation = () => {
-  if (projectElements) {
-    const isVisible = (element) => {
-      const elementBoundary = element.getBoundingClientRect();
-      const top = elementBoundary.top;
-      const bottom = elementBoundary.bottom;
-      return (top >= 0) && (bottom - Y_TRANSLATION <= screenHeight);
-    };
+const isVisible = (element) => {
+  const elementBoundary = element.getBoundingClientRect();
+  const top = elementBoundary.top;
+  const bottom = elementBoundary.bottom;
+  return (top >= 0) && (bottom - Y_TRANSLATION <= screenHeight);
+};
 
-    projectElements.forEach((element) => {
+const animateAppearance = () => {
+  if (animateAppearanceElements) {
+    animateAppearanceElements.forEach((element) => {
       if (isVisible(element)) {
         element.classList.add('is-active');
       }
     });
   }
+};
+
+const animateHeader = () => {
+  if (headerElement) {
+    const scrollTop = window.scrollY;
+    const delta = scrollTop - scrollStarted;
+    // console.log(scrollTop, SCROLLED_HEADER_START, delta);
+    headerElement.classList.toggle('main-header--sticky', (scrollTop >= SCROLLED_HEADER_START) && (delta > 0));
+    scrollStarted = scrollTop;
+  }
+};
+
+const initScrollAnimation = () => {
+  animateAppearance();
 
   const onWindowScroll = () => {
-    if (headerElement) {
-      const scrollTop = window.scrollY;
-      const delta = scrollTop - scrollStarted;
-      // console.log(scrollTop, SCROLLED_HEADER_START, delta);
-      headerElement.classList.toggle('main-header--sticky', (scrollTop >= SCROLLED_HEADER_START) && (delta > 0));
-      scrollStarted = scrollTop;
-    }
-
-    if (projectElements) {
-      const isFullyVisible = (element) => {
-        const elementBoundary = element.getBoundingClientRect();
-        const top = elementBoundary.top;
-        const bottom = elementBoundary.bottom;
-        return (top >= 0) && (bottom <= screenHeight);
-      };
-
-      projectElements.forEach((element) => {
-        if (isFullyVisible(element)) {
-          element.classList.add('is-active');
-        }
-      });
-    }
+    animateHeader();
+    animateAppearance();
   };
 
   window.addEventListener('scroll', onWindowScroll);
